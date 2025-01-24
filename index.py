1f4e416e00756a6922ca8d2f4c47275e76ce9d68
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-24 09:59:58 +0500

    Update index.py to display a welcome message and decorative lines for Python programming

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("hello world..................")
+welcome = "Welcome to Python Programming Language for AI."
+print(welcome)
+
+line01 = "********************"  
+line02 = "*                  *" 
+line03 = "*  PYTHON FOR AI   *"
+
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)

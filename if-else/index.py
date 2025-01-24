bcdc49df8c84709194580b9e8383b784518bc713
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-24 10:16:21 +0500

    if-else added

---
+++ if-else/index.py
@@ -0,0 +1,8 @@
+number=20
+
+if number>150:
+    print("Number is greater than 15")
+else:
+    print("Number is less than 15")
+
+print("Out of if statement") if number>15 else print("Out of else statement")

96aed924187cbd94e1e13d45add31a620f11bbc0
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-24 10:58:33 +0500

    tries different sting methods

---
+++ data_types/index.py
@@ -0,0 +1,34 @@
+
+firstName="Muhammad"
+lastName="Ali"
+# fullName="Muhammad Ali                 khan"
+# print(len(fullName.strip()))
+# print(fullName.strip())
+# print(len(fullName.strip()))
+print(firstName.startswith("Mu"))
+print(firstName.endswith("ad"))
+print(firstName[0:-1])
+
+# print(firstName.lower())
+# print(firstName.upper())
+# print(len(firstName))
+# print(len(lastName))
+
+# print(type(firstName))
+# print(type(firstName)==str)
+# print(isinstance(firstName,str))
+
+# Concatenation
+# fullName=firstName+" "+lastName
+# print(fullName)
+
+# casting a number to string
+# age=20
+# print(type(age))
+# print(age)
+# age=str(age)
+# print(type(age))
+# print(age)
+
+# print("My name is "+fullName+" and I am "+age+" years old.")
+

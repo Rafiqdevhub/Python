7bfbc0c6a2827e6dadc79889e2e3217547f24b16
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-25 11:05:00 +0500

    Add tuple examples and unpacking demonstration

---
+++ tuples/main.py
@@ -0,0 +1,12 @@
+myTuples=tuple((1,2,3,4,5))
+names=tuple(('user1','user2','user3','user4','user5'))
+# print(myTuples)
+# print(names)
+
+values=(1,2,3,4,5)
+
+(x,y,*z, h)=values
+print(x)
+print(y)
+print(z)
+print(h)

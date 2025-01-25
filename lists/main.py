b7cec1e8b1543acc5a00dc54de19e27443da03bb
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-25 10:55:30 +0500

    list added with different examples

---
+++ lists/main.py
@@ -0,0 +1,66 @@
+users=['admin','user1','user2','user3','user4','user8','user7','user6','User5']
+
+students=['student1','student2','student3','student4','student5']
+
+teachers=['teacher1','teacher2','teacher3','teacher4','teacher5']
+
+# print('admin' in users)
+
+# print(students[-1])
+# print(teachers.index('teacher3'))
+# print(users[1:3])
+# print(users[1:])
+
+# print(len(users))
+
+# users.append('user5')
+# print(users)
+
+# users = users + ['user6','user7']
+# print(users)
+
+# users.extend(['user6','user7'])
+# print(users)
+
+# users.extend(students)
+# print(users)
+
+
+# users.insert(1,'user0')
+# print(users)
+
+# users[1:1]=['user-1','user-2']
+# print(users)
+
+# users[1:3]=['user-1','user-2']
+# print(users)
+
+# users.remove('user1')
+# print(users)
+
+# users.pop(1)
+# print(users)
+
+# del users[2]
+# print(users)
+
+# users.clear()
+# print(users)
+
+# users.sort()
+# print(users)
+
+# users.sort(key=str.lower)
+# print(users)
+
+
+# numbers=[1,2,3,4,5,6,7,8,9,10,12,22,22222,22222,89]
+#  numbers.sort()
+# numbers.sort(reverse=True)
+#  print(numbers)
+# print(sorted(numbers, reverse=True))
+
+# print(type(numbers))
+
+new_list=list([1,2,3,4,5,6,7,8,9,10,12,22,22222,22222,89])
+print(new_list)

568e1c1548440a68b1477657eef87012821e15a2
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-26 11:59:36 +0500

    Add examples demonstrating set creation and operations

---
+++ sets/main.py
@@ -0,0 +1,37 @@
+# numbers={2,3,4,5,6,7,8,9,10}
+
+
+# print(numbers)
+# print(type(numbers))
+
+# new_numbers = set((1,2,3,4,5,6,7,8,9,10))
+# print(new_numbers)
+# print(type(new_numbers))
+# print(len(new_numbers))
+
+
+# no duplicates in set
+# numbers={1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10}
+# print(numbers)
+
+# bool_set={True,False,True,False}
+# bool_set={0, False, 1, True}
+# bool_set={False, True}
+# print(bool_set)
+
+# print(1 in numbers)
+
+# add element to set
+# numbers.add(11)
+# print(numbers)
+
+# numbers.update(bool_set)
+# print(numbers)
+
+numbers={1,2,3,4,5,6,7,8,9,10}
+new_numbers ={1,11, 12, 13, 14, 15}
+print(numbers)
+
+union_set = numbers.union(new_numbers)
+# numbers.intersection_update(new_numbers)
+# print(numbers)

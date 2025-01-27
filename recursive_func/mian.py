476fdb64dd9c4f2e70f42c8ce0af4b5cb183f596
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-27 11:12:31 +0500

    Add recursive function example and while loop demonstration

---
+++ recursive_func/mian.py
@@ -0,0 +1,31 @@
+
+# def add_one(number):
+#     if (number>=9):
+#         return number + 1
+#     total = number + 1
+#     print(total)
+
+#     return add_one(total)
+
+# # add_one(0)
+# total_number = add_one(0)
+# print(total_number)
+
+# value="Hell"
+
+# while value:
+#     print(value)
+#     value = 0
+
+
+val="Hello"
+count=0
+
+while val:
+    count+=1
+    print(count)
+    if count==5:
+        break
+    else:
+        val=0
+        continue

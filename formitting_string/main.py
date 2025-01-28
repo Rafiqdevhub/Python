d149bb860e0bd94ab7f228edde82566d7487ce4b
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-28 10:50:56 +0500

    Add string formatting examples using f-strings and format method

---
+++ formitting_string/main.py
@@ -0,0 +1,46 @@
+name="Ali"
+age=25
+
+
+# full_name=( first_name+" "+last_name)
+
+# full_name="\n%s is %s old." % (name, age)
+
+# full_name="\n{} is {} old." .format(name, age)
+
+# full_name="\n{1} is {0} old." .format(age, name)
+
+# full_name="\n{name} is {age} old." .format(name=name, age=age)
+
+# f_string
+
+# full_name=f"\n{name} is {age} old. \n"
+
+# full_name=f"\n{name} is {23+2} old. \n"
+
+# full_name=f"\n{name.lower()} is {2*12+1} old. \n"
+
+# print(full_name)
+
+# print(full_name)
+
+# player={"name":"Ali", "age":25}
+
+# data="{name} is {age} old." .format(**player)
+
+# print(data)
+
+# message=f"{player['name']} is {2*10+5} old."
+
+# print(message)
+
+
+num=12
+
+# print(f"2.25 times {num} is {2.25 * num:.2f}\n")
+
+# for num in range(1, 13):
+#      print(f"2.25 times {num} is {2.25 * num:.2f}")
+
+for num in range(1, 13):
+     print(f"{num} is divide by 11.23 {num/11.23:.2%}")

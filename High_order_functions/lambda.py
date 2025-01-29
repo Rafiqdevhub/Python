897f85c3c169c2dda127be090a20d9646f9749f7
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-29 14:28:40 +0500

    Add examples of lambda functions and high-order functions in lambda.py

---
+++ High_order_functions/lambda.py
@@ -0,0 +1,18 @@
+# squired=lambda num: num**2
+
+# print(squired(5))
+
+# sum=lambda a,b: a+b
+
+# print(sum(5,6))
+
+
+# High  order function with lambda (function that takes function as argument)
+# def my_func(n):
+#   return lambda a : a * n
+
+# add_5=my_func(5)
+# add_10=my_func(10)
+
+# print(add_5(10))
+# print(add_10(10))

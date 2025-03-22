7763430536b595296194f695a2868b9adc9b911c
MUhammad Rafiq <rafiq85825@gmail.com>
2025-03-22 15:58:26 +0500

    Refactor main.py to comment out array print statements and improve code readability

--- numpy/main.py
+++ numpy/main.py
@@ -2,12 +2,21 @@ import numpy as np
 
 # Create a 1D array
 arr_1d = np.array([1, 2, 3, 4, 5])
-print("1D Array:", arr_1d)
+# print("1D Array:", arr_1d)
 
 # Create a 2D array
 arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
-print("2D Array:\n", arr_2d)
+# print("2D Array:\n", arr_2d)
 
 # Create a 3D array
 arr_3d = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
-print("3D Array:\n", arr_3d)
+dimension = arr_3d.ndim
+# print("Number of dimensions:", dimension)
+# print("3D Array:\n", arr_3d)
+
+
+heights = np.random.randint(150, 200, size=100000)
+
+# Calculating the average height
+average_height = np.mean(heights)
+print("Average Height:", average_height)
